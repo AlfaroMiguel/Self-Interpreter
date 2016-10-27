080c2f29e3d97928ded53ac41633e0be31ccde74
kurepa <cristian3629@gmail.com>
2016-10-27 14:12:57 -0300

    agregué comandos de debug al compilar

--- src/server/ejecutar.sh
+++ src/server/ejecutar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 `flex scanner.l`
-`bison -d  --debug -g -t parser.y`
+`bison -d  --debug -g -tx parser.y`
 
 g++ -std=c++11 -o parser  -Wno-write-strings interpreter.cpp lex.yy.c parser.tab.c
 echo 'Parse init'
