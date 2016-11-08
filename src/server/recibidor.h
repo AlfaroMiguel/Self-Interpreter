724996be4ecdcc7c55b1221d02d083e819d6b5f6
Alfaro Miguel <miguelalfaro95@hotmail.com>
2016-11-08 16:56:57 -0300

    Update recibidor.h

--- src/server/recibidor.h
+++ src/server/recibidor.h
@@ -1,8 +1,21 @@
-//
-// Created by mikealpha on 08/11/16.
-//
+#ifndef RECIBIDOR_H
+#define RECIBIDOR_H
 
-#ifndef SELFTP_RECIBIDOR_H
-#define SELFTP_RECIBIDOR_H
+#include "../common/thread.h"
+#include "../common/socket.h"
+#include "comunicador_cliente.h"
 
-#endif //SELFTP_RECIBIDOR_H
+class Recibidor: public Thread{
+ public:
+  Recibidor(Socket& skt, ComunicadorCliente& com_cliente);
+  ~Recibidor();
+  void run() {
+	  recibir();
+  }
+ private:
+  	Socket& skt;
+  	ComunicadorCliente& com_cliente;
+  	bool esta_recibiendo;
+  	void recibir();
+};
+#endif
