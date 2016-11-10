5352d7322f1d397588685ff86ba3b5fca7e26121
kurepa <cristian3629@gmail.com>
2016-11-10 16:03:51 -0300

    actualizo el main

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -4,9 +4,18 @@
 
 int main(int argc, char const *argv[]) {
   Interpreter interpreter;
-  char* buffer = "lobby _AddSlots: (| objeto = (| x = 1. y = 3. metodo = (|  | 3 * 2 + 1. ). |).  |).";
-  interpreter.interpret(buffer);
-  char* otro = "objeto metodo.";
-  interpreter.interpret(otro);
+  //interpreter.interpretFile(argv[1]);
+  char pajaro[] = "lobby _AddSlots: (| pajaro = (| x = 1. y = 3. algunMetodo = (|  | 3 * 2 + 1. ). |).  |). \n\0\0";
+  interpreter.interpretChar(pajaro);
+  char algunMetodo[] = "pajaro algunMetodo.\n\0\0";
+  interpreter.interpretChar(algunMetodo);
+  char OtroObjeto[] = "lobby _AddSlots: (| OtroObjeto = (|x = 1. y <- 3. metodo = (|  | 4 + 2 + 1. ).|).|).";
+  char objeto[] = "lobby _AddSlots: (| objeto = (| x = 1. y = 3. numero = (|  | 3 * 3.).|). |).";
+  char numero[] = "objeto numero.";
+  char metodo[] = "OtroObjeto metodo.";
+  interpreter.interpretChar(OtroObjeto);
+  interpreter.interpretChar(objeto);
+  interpreter.interpretChar(numero);
+  interpreter.interpretChar(metodo);
   return 0;
 }
