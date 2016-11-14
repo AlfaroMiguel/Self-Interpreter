0c2d5a9ad26fa5c1ef5f12ad91ce1ab50f98b9e4
Miguel Alfaro <miguel.alfaro.a95@gmail.com>
2016-11-14 17:07:57 -0300

    Elimino archivos innecesarios, empiezo merge Server-Cliente

--- src/server/parser/ejecutar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-`flex --header-file=scanner.h scanner.l`
-`bison -d  --debug -g -v parser.y`
-
-echo `g++ -Wall -std=c++11 -ggdb -o parser  -Wno-write-strings *.c  *.cpp ../*.cpp`
-echo 'Parse init'
-./parser  __stdin__
