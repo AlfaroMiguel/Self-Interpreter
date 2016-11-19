9708778b0fbb4181243be0a26a390f9ee088ca92
Miguel Alfaro <miguel.alfaro.a95@gmail.com>
2016-11-19 19:11:43 -0300

    Actualizo scripts

--- install.sh
+++ install.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 cd src/server/parser
+
+echo '#######################################################'
+echo 'GENERANDO PARSER SELF'
+echo '#######################################################'
 ./generar.sh
-echo 'Parser generado'
+echo '#######################################################'
+echo 'PARSER GENERADO CORRECTAMENTE'
+echo '#######################################################'
 cd ../../..
 cmake ./src
 cp src/client/ventana_vm.glade client/ventana_vm.glade
+echo 'INSTALANDO SELF INTERPRETER'
 make
-echo 'Interprete Self instalado'
+echo '#######################################################'
+echo 'SELF INTERPRETER INSTALADO CORRECTAMENTE'
+echo '#######################################################'
+echo 'INICIAR SERVER: en "/server" ejecutar "./server port"'
+echo '#######################################################'
+echo 'INICIAR CLIENTE: en "/client" ejecutar "./client host port"'
+echo '#######################################################'
+echo 'ULTIMA VERSION DISPONIBLE EN: https://github.com/AlfaroMiguel/Self-Interpreter'
+echo '#######################################################'
+echo 'REALIZADO POR: Alfaro Miguel - Gonzalez Cristian - Mendez Agustina'
