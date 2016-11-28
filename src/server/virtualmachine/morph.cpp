60b17898edff8025c2945bddc642fe697b53e400
Miguel Alfaro <miguel.alfaro.a95@gmail.com>
2016-11-27 23:15:22 -0300

    Muevo serializacion morph

--- src/server/virtualmachine/morph.cpp
+++ src/server/virtualmachine/morph.cpp
@@ -58,3 +58,28 @@ std::string Morph::getEvent(const std::string& eventName){
 int Morph::getId(){
     return id;
 }
+
+/*Serializacion*/
+
+void Morph::serialize(json& jserialization){
+    jserialization["name"] = name;
+    jserialization["x"] = x;
+    jserialization["y"] = y;
+
+    json jSlots;
+    for(auto itSlot = slots.begin(); itSlot != slots.end(); itSlot++){
+        jSlots.push_back(std::make_pair(itSlot->first, itSlot->second));
+    }
+    jserialization["slots"] = jSlots;
+    jserialization["id"] = id;
+}
+
+void Morph::deserialize(json& jdeserialization){
+    this->setName(jdeserialization["name"]);
+    this->changePosition(jdeserialization["x"], jdeserialization["y"]);
+    json jSlots = jdeserialization["slots"];
+    for(auto itSLots = jSlots.begin(); itSLots != jSlots.end(); itSLots++){
+        this->addSlot(itSLots.key(), itSLots.value());
+    }
+    this->id = jdeserialization["id"];
+}
--- src/server/virtualmachine/morph.h
+++ src/server/virtualmachine/morph.h
@@ -25,32 +25,6 @@ private:
     std::map<std::string, std::string> slots;
     int id;
 public:
-
-    //Serialization TEST
-
-    void serialize(json& jserialization){
-        jserialization["name"] = name;
-        jserialization["x"] = x;
-        jserialization["y"] = y;
-
-        json jSlots;
-        for(auto itSlot = slots.begin(); itSlot != slots.end(); itSlot++){
-            jSlots.push_back(std::make_pair(itSlot->first, itSlot->second));
-        }
-        jserialization["slots"] = jSlots;
-        jserialization["id"] = id;
-    }
-
-    void deserialize(json& jdeserialization){
-        this->setName(jdeserialization["name"]);
-        this->changePosition(jdeserialization["x"], jdeserialization["y"]);
-        json jSlots = jdeserialization["slots"];
-        for(auto itSLots = jSlots.begin(); itSLots != jSlots.end(); itSLots++){
-            this->addSlot(itSLots.key(), itSLots.value());
-        }
-        this->id = jdeserialization["id"];
-    }
-
     static int idNumber;
     /*Crea un morph, se le setea un unico id*/
     Morph();
@@ -70,6 +44,10 @@ public:
     std::string getEvent(const std::string &eventName);
     /*Devuelve el id del morph*/
     int getId();
+
+    /*Serializacion*/
+    void serialize(json& jserialization);
+    void deserialize(json& jdeserialization);
 };
 
 #endif
